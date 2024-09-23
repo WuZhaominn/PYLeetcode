1b803115ac61d54c113582ef3a8aeb9a36591f58
WuZhaominn <145826961+WuZhaominn@users.noreply.github.com>
2024-09-23 20:54:31 +0800

    Update 两数之和.py
    加方法

--- 两数之和.py
+++ 两数之和.py
@@ -1,3 +1,4 @@
+方法1：
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         lenth=len(nums)
@@ -5,4 +6,25 @@ class Solution:
             for j in range(i+1,lenth):
                 if nums[i]+nums[j]==target:
                     return [i,j]
-
+方法2：哈希表
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash={}
+        for index,num in enumerate(nums):
+            try:
+                hash[num].append(index)
+            except KeyError:
+                hash[num]=[index]
+        for index,num in enumerate(nums):
+            another=target-num
+            try:
+                index1=hash[another]
+                if another ==num:
+                    if len(index1)>1:
+                        return index1
+                    else:
+                        continue
+                else:
+                    return [index,index1[0]]
+            except KeyError:
+                pass   
