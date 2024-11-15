75c0b9473d4378ffe63a940ed32f52d35db42ba7
WuZhaominn <145826961+WuZhaominn@users.noreply.github.com>
2024-11-15 13:29:36 +0800

    Update 和为k的子数组.py

--- 和为k的子数组.py
+++ 和为k的子数组.py
@@ -8,6 +8,7 @@ class Solution:
         for num in nums:
             curr_sum +=num
             # Check if sum - k in hash
+            #curr_sum - k 的含义是：从当前位置往回看，是否存在一个累积和等于 curr_sum - k。如果 curr_sum - k 存在于 sum_map 中，说明在当前位置和前面的某一个位置之间的子数组和为 k。因此，将 sum_map[curr_sum - k] 的值加到 count 上（即累计和为 curr_sum - k 的出现次数），统计满足条件的子数组数量。
             count += sum_map.get(curr_sum - k ,0)
             # add curr_sum to hash
             sum_map[curr_sum] = sum_map.get(curr_sum,0) + 1
