7392e2ba685755f666cc7d876ae0f1bb1f543b8a
WuZhaominn <145826961+WuZhaominn@users.noreply.github.com>
2025-01-06 21:05:37 +0800

    Update 环形链表.py

--- 环形链表.py
+++ 环形链表.py
@@ -3,14 +3,11 @@ class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         
         if not head:
-            slow, fast = None, None
-        else:
-            slow, fast = head, head.next
-        while slow != None and fast != None:
+            return False
+        slow, fast = head,head
+        while fast and fast.next :
+            slow, fast = slow.next, fast.next.next 
             if slow == fast:
                 return True
-            if not fast.next:
-                slow, fast = None, None 
-            else:
-                slow, fast = slow.next, fast.next.next 
+                
         return False
