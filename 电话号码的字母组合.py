fc8216858594a4e82801961cfd7111555cfa7ae7
WuZhaominn <145826961+WuZhaominn@users.noreply.github.com>
2025-02-27 23:16:31 +0800

    Update 电话号码的字母组合.py

--- 电话号码的字母组合.py
+++ 电话号码的字母组合.py
@@ -2,8 +2,9 @@ class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         if not digits:
             return []
-        #数字到字母的映射
-        dig_char ={
+        
+        # 数字到字母的映射
+        dig_char = {
             "2": "abc",
             "3": "def",
             "4": "ghi",
@@ -14,21 +15,21 @@ class Solution:
             "9": "wxyz",
         }
 
-
-        def backtracking(index):
-            if index==len(digits):
-            #将当前的字母组合（res1）添加到结果列表 res 中
-                a="".join(result)
-                results.append(a)
-            else:
-                #加入输入2，3，那第一轮的时候digit就为2
-                digit = digits[index]
-                #i在abc中
-                for i in dig_char[digit]:
-                    result.append(i)
-                    backtracking(index+1)
-                    result.pop()
         result = []
         results = []
-        backtracking(0)
+        self.backtracking(digits, dig_char, 0, result, results)
         return results
+
+    def backtracking(self, digits, dig_char, index, result, results):
+        if index == len(digits):
+            # 将当前的字母组合（result）添加到结果列表 results 中
+            a = "".join(result)
+            results.append(a)
+        else:
+            # 获取当前数字对应的字母
+            digit = digits[index]
+            # 遍历当前数字对应的所有字母
+            for i in dig_char[digit]:
+                result.append(i)
+                self.backtracking(digits, dig_char, index + 1, result, results)
+                result.pop()
