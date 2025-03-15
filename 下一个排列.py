c67f5c7f36cae56a4d5033dfe5c36a7d90d78cd1
WuZhaominn <145826961+WuZhaominn@users.noreply.github.com>
2025-03-15 18:13:48 +0800

    Create 下一个排列.py

---
+++ 下一个排列.py
@@ -0,0 +1,22 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i = len(nums)-1
+
+        while i>0 and nums[i-1] >=nums[i]:
+            i -= 1
+        if i !=0:
+            j= len(nums)-1
+
+            while nums[j] <= nums[i-1]:
+                j-=1
+            nums[i-1] ,nums[j] = nums[j],nums[i-1]
+
+        left,right =i,len(nums)-1
+
+        while left<right:
+            nums[left] ,nums[right] = nums[right],nums[left]
+            left +=1
+            right -=1
